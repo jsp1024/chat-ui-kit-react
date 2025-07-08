e8e8ccbae1e94512d7c90d8ab7d11c552c088a66
Jaeman <46260954+jsp1024@users.noreply.github.com>
2025-07-08 02:11:43 +0000

    feat

--- src/components/ContentEditable/ContentEditable.jsx
+++ src/components/ContentEditable/ContentEditable.jsx
@@ -124,7 +124,6 @@ export class ContentEditable extends Component {
         ref={msgRef}
         className={className}
         contentEditable={contentEditable}
-        disabled={disabled}
         data-placeholder={ph}
         onInput={handleInput}
         onKeyPress={handleKeyPress}
